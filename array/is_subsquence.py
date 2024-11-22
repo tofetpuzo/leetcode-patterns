330386562662b27073e2f8cab3c8350976504109
tofetpuzo <tofet.femi@gmail.com>
2024-11-22 18:11:21 +0000

    array patterns

--- array/is_subsquence.py
+++ array/is_subsquence.py
@@ -6,6 +6,7 @@
 
 # A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
+
 # Example 1:
 
 # Input: s = "abc", t = "ahbgdc"
@@ -24,6 +25,11 @@
  
 
 # Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
-
-def is_subsquence(s, t):
-    i_
+def isSubsequence(s: str, t: str) -> bool:
+    i = 0
+    j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)
