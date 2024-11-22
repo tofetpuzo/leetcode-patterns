2226611f42db6663e1340a33c808ea940819a7fd
tofetpuzo <topetemiola09@gmail.com>
2024-11-22 20:40:31 +0000

    best_time_to_buy, longest_common_prefix, summary_ranges

---
+++ array/best_time_to_buy_coin.py
@@ -0,0 +1,59 @@
+# 121. Best Time to Buy and Sell Stock
+# Easy
+# Topics
+# Companies
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+# Example 1:
+
+# Input: prices = [7, 1, 5, 3, 6, 4]
+# Output: 5
+# Explanation: Buy on day 2 (price=1) and sell on day 5 (price=6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+# Example 2:
+
+# Input: prices = [7, 6, 4, 3, 1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+# Constraints:
+
+# 1 <= prices.length <= 105
+# 0 <= prices[i] <= 104
+
+def maxProfit(prices) -> int:
+    """
+        :type prices: List[int]
+        :rtype: int
+        """
+    max_profit = 0
+    day_price = prices[0]
+    for num in prices:
+        min_price_seen_so_far = min(day_price, num) 
+        day_price = min_price_seen_so_far
+        if min_price_seen_so_far < num:
+            total_buy = num - min_price_seen_so_far
+            max_profit = max(max_profit, total_buy)
+        else:
+            max_profit = max_profit
+    return max_profit
+
+# Time: O(n)
+# Space: O(1)
+
+
+# Test case
+prices = [7, 1, 5, 3, 6, 4]
+
+print(maxProfit(prices))  # 5
+
+# Test case
+prices = [7, 6, 4, 3, 1]
+
+print(maxProfit(prices))  # 0
