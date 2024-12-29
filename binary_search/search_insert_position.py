30e0ef7faf1995b14f1fb226749331cc548815c8
tofetpuzo <topetemiola09@gmail.com>
2024-12-29 17:56:52 +0000

    binary search problems

--- binary_search/search_insert_position.py
+++ binary_search/search_insert_position.py
@@ -32,9 +32,35 @@
 # -104 <= target <= 104
 
 
-def searchInsert(self, nums, target):
+def searchInsert(nums, target):
     """
     :type nums: List[int]
     :type target: int
     :rtype: int
     """
+
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return l
+
+# Time complexity: O(log n)
+# Space complexity: O(1)
+
+# test case
+nums = [1, 3, 5, 6]
+target = 5
+print(searchInsert(nums, target))  # 2
+
+
+nums2 = [1, 3, 5, 6]
+target2 = 2
+print(searchInsert(nums2, target2))  # 1
+
