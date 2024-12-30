487452c766c9312458c0ddbd0249c66bdbe2e8c0
tofetpuzo <tofet.femi@gmail.com>
2024-12-30 09:33:03 +0000

    search rotated soted

--- binary_search/search_in_rotated_sorted_array.py
+++ binary_search/search_in_rotated_sorted_array.py
@@ -22,4 +22,42 @@
 # Output: -1
 # Example 3:
 
+
 # Input: nums = [1], target = 0
+def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+
+    l = 0
+    r = len(nums) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[l] <= nums[mid]:
+            if nums[l] <= target <= nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if nums[mid] <= target <= nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return -1
+
+
+# Time complexity: O(log n)
+# Space complexity: O(1)
+
+# test case
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 0
+print(search(nums, target))  # 4
+# constraints:
+# 1 <= nums.length <= 5000
+# -104 <= nums[i] <= 104
+# All values of nums are unique.
