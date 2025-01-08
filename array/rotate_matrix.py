2a6519bc1b53befa3beb10cfca2c86206210a5b7
tofetpuzo <topetemiola09@gmail.com>
2025-01-08 11:12:13 +0000

    updated the rotate matrix

--- array/rotate_matrix.py
+++ array/rotate_matrix.py
@@ -36,3 +36,23 @@ def rotate(matrix):
             matrix[col][end-row] = temp
 
     return matrix
+
+
+def rotate_2(matrix):
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        for j in range(n//2):
+            matrix[i][j], matrix[i][n - j -
+                                    1] = matrix[i][n - j - 1], matrix[i][j],
+
+    return matrix
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(rotate_2(matrix))
