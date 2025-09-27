7428ce2cd2b36f2e3c70671f1017c2a13ff332f2
tofetpuzo <tofet.femi@gmail.com>
2025-09-27 15:40:56 +0100

    150 questions

---
+++ 150_questions/back_tracking/permutation.py
@@ -0,0 +1,42 @@
+# 46. Permutations
+# Medium
+# Topics
+# conpanies icon
+# Companies
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+
+# Input: nums = [1]
+# Output: [[1]]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 6
+# -10 <= nums[i] <= 10
+# All the integers of nums are unique.
+
+class Solution:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        def backtrack(start: int):
+            if start == len(nums):
+                result.append(nums[:])
+                return
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+        result = []
+        backtrack(0)
+        return result
